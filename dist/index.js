6b8a312dfbed6a68d3c611c01e745a1d6310d8e9
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2021-06-23 14:42:29 +0200

    remove dist folder

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.doAnything = void 0;
-console.info('TODO!');
-function doAnything() {
-    console.info('doing anything...');
-}
-exports.doAnything = doAnything;
-//# sourceMappingURL=index.js.map
--- dist/index.spec.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-describe('x', function () {
-    test('y', function () {
-        expect(1 + 1).toEqual(2);
-    });
-});
-//# sourceMappingURL=index.spec.js.map
