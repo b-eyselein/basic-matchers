28aedf74e3f4acba8e971aebb103c343faf5ffbf
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2021-06-23 10:36:49 +0200

    update build

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.doAnything = void 0;
+console.info('TODO!');
+function doAnything() {
+    console.info('doing anything...');
+}
+exports.doAnything = doAnything;
+//# sourceMappingURL=index.js.map
